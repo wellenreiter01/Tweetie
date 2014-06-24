5cd9b481f57b793fb611fb1e6dfc6452e02fee1b
wellenreiter01 <wellenreiter01@users.noreply.github.com>
2014-06-24 16:29:28 +0200

    Update Version.php

--- lib/Tweetie/Base/Version.php
+++ lib/Tweetie/Base/Version.php
@@ -34,7 +34,7 @@ class Tweetie_Base_Version extends Zikula_AbstractVersion
         $meta['url']                  = $this->__('tweetie');
         // core requirement
         $meta['core_min']             = '1.3.5'; // requires minimum 1.3.5
-        $meta['core_max']             = '1.3.6'; // not ready for 1.3.6 yet
+        $meta['core_max']             = '1.3.9'; // not ready for 1.3.6 yet
 
         // define special capabilities of this module
         $meta['capabilities'] = array(
